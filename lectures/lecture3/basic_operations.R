7625336ada576aa0e4da2ac69455e071e8675d75
Camron Khan <camron.khan@gmail.com>
2017-01-31 13:07:05 -0600

    lecture 3 notes

---
+++ lectures/lecture3/basic_operations.R
@@ -0,0 +1,25 @@
+"Hello World!"
+2/3 + 4*5
+log(2)
+exp(10)
+log10(10)
+log(15, base=15)
+sqrt(10)
+options(digits=16)
+sqrt(10)
+1:500
+x <- 1 + 2
+x
+f <- function(x){x+1}
+f(1)
+f(10)
+1 + 2/5 -> y
+y
+p <- c(1,2)
+p
+x <- 1
+y <- 2
+z <- c(x, y, p)
+z
+assign("x", 2*3+5)
+x
