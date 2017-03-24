6fbb4da2a73a77b226256a088c1fc2e4703aa0ba
Camron Khan <camron.khan@gmail.com>
2017-03-23 20:42:21 -0500

    wordcloud as png

--- hw/hw3/script.R
+++ hw/hw3/script.R
@@ -15,10 +15,10 @@ library(SnowballC)
 library(wordcloud)
 library(RColorBrewer)
 
-train_ham_dir = "../../../data/ml_hw3/hw3_train/train/ham/"
-train_spam_dir = "../../../data/ml_hw3/hw3_train/train/spam/"
-test_ham_dir = "../../../data/ml_hw3/hw3_test/test/ham/"
-test_spam_dir = "../../../data/ml_hw3/hw3_test/test/spam/"
+train_ham_dir = "./data.gi/train/ham/"
+train_spam_dir = "./data.gi/train/spam/"
+test_ham_dir = "./data.gi/test/ham/"
+test_spam_dir = "./data.gi/test/spam/"
 
 train_ham_corpus = VCorpus(DirSource(directory = train_ham_dir, encoding = "UTF-8"))
 train_spam_corpus = VCorpus(DirSource(directory = train_spam_dir, encoding = "UTF-8"))
@@ -45,6 +45,10 @@ train_spam_dtm <- DocumentTermMatrix(train_spam_corpus, control = list(tolower =
 test_ham_dtm <- DocumentTermMatrix(test_ham_corpus, control = list(tolower = TRUE, removeNumbers = TRUE, removePunctuation = TRUE, stopwords = customStopWords, stemming = TRUE))
 test_spam_dtm <- DocumentTermMatrix(test_spam_corpus, control = list(tolower = TRUE, removeNumbers = TRUE, removePunctuation = TRUE, stopwords = customStopWords, stemming = TRUE))
 
+
+
+test_spam_dtm
+
 #===========================================================================================================================================
 
 # WORD CLOUDS
@@ -55,14 +59,15 @@ train_ham_matrix <- as.matrix(train_ham_dtm)
 train_ham_freqs <- sort(colSums(train_ham_matrix), decreasing = TRUE)
 train_ham_words <- names(train_ham_freqs)
 train_ham_df <- data.frame(word = train_ham_words, freq = train_ham_freqs)
-train_ham_wc <- wordcloud(words = train_ham_df$word, freq = train_ham_df$freq, max.words = 50, min.freq = 2, rot.per = 0.15, scale = c(3, 0.5), random.order = FALSE, colors = colorPalette)
+png("train_ham_wc.png", width=1280,height=800)
+wordcloud(words = train_ham_df$word, freq = train_ham_df$freq, max.words = 50, min.freq = 2, rot.per = 0.15, scale = c(3, 0.5), random.order = FALSE, colors = colorPalette)
+dev.off()
 
 train_spam_matrix <- as.matrix(train_spam_dtm)
 train_spam_freqs <- sort(colSums(train_spam_matrix), decreasing = TRUE)
 train_spam_words <- names(train_spam_freqs)
 train_spam_df <- data.frame(word = train_spam_words, freq = train_spam_freqs)
-train_spam_wc <- wordcloud(words = train_spam_df$word, freq = train_spam_df$freq, max.words = 50, min.freq = 2, rot.per = 0.15, scale = c(3, 0.5), random.order = FALSE, colors = colorPalette)
-
-
-
+png("train_spam_wc.png", width=1280,height=800)
+wordcloud(words = train_spam_df$word, freq = train_spam_df$freq, max.words = 50, min.freq = 2, rot.per = 0.15, scale = c(3, 0.5), random.order = FALSE, colors = colorPalette)
+dev.off()
 
